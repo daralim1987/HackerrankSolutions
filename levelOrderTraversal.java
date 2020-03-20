1c4713cd4f75a65dc21bb288e13b4e94b1d67d31
daralim <daralim97@yahoo.com>
2020-03-20 09:51:04 -0500

    Function to printlevel order of binary tree

---
+++ levelOrderTraversal.java
@@ -0,0 +1,16 @@
+public static void levelOrder(Node root){
+	Queue<Node>q = new LinkedList<>();
+	q.add(root);
+	while(!q.isEmpty()){
+		Node temp = q.poll();
+		System.out.print(temp.data + " ");
+		if(temp.left != null){
+			q.add(temp.left);
+		}
+		if(temp.right != null){
+			q.add(temp.right);
+		}
+	}
+
+}
+
